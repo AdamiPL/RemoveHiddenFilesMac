648d17371edc365191f1207d793ffcef3d921dda
AdamiPL <reivendj@gmail.com>
2015-03-09 10:26:48 +0000

    Create clean.sh

---
+++ clean.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Created by AdamiPL @ TheWebSites.uk
+# For Mac Users
+# www.hamac.pl
+
+
+if [ $1 ]
+then
+	echo "Selected path $1"
+	echo "is correct? (y/n)"
+	read d
+	
+	case "$d" in
+		"y") sudo find $1 \( -name '.DS_Store' -or -name '.Trashes' -or -name '.fseventsd' -or -name '._.DS_Store' -or -name '.TemporaryItems' -or -name '.apdisk' -or -name '._*' \) -exec rm -rf {} \; ;; 
+		"n") exit ;;
+		*) echo "Nothing chosen..."
+	esac
+
+else
+	echo "Enter the path to scan eg: ./clean /Volumes/Dane/";
+fi
